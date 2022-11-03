96ca8c33fc362173a14506034b401b2950fed544
sonali <ahirwarsonu0709@gmail.com>
2022-11-04 00:47:07 +0530

    modified

--- Test1.java
+++ Test1.java
@@ -1,5 +1,16 @@
 public class Test1{
-public void add()
-System.out,println("add method");
+<<<<<<< Updated upstream
+public void add(){
+
+System.out.println("add method");
+
+=======
+public static void main(String[ ] args){
+int a=10;
+int b=20;
+int c=a+b;
+Sysem.out.println(c);
+>>>>>>> Stashed changes
+}
 }
 }
